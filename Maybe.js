3f67a4115f2198970d92236a350f34a41289923b
Tim Biddington <timbiddington@fastmail.com.au>
2022-03-07 15:35:43 +1100

    Merge disparate object for Maybe

--- Maybe.js
+++ Maybe.js
@@ -5,36 +5,33 @@
 /**
  * Serves as the base struct for our type
  */
-const maybe = {
+const Maybe = {
+
+  of: value => {
+    return value !== null ? Some(value) : Nil()
+  },
+
   unwrap: function () {
     return this.value;
   },
+
   map: function (fn) {
-    return this.kind == "Nil" ? Nil() : Maybe.of(fn(this.value));
+    return this.kind === "Nil"
+      ? Nil()
+      : Maybe.of(fn(this.value));
   }
 }
 
 const Some =
-  value => {
-    return Object.assign({
-      kind: "Some",
-      value,
-    }, maybe)
-  }
+  value =>
+    Object.assign({ kind: "Some", value }, Maybe)
 
 const Nil =
-  () => {
-    return Object.assign({
-      kind: "Nil"
-    }, maybe)
-  }
-
-const Maybe = {
-  of: value => {
-    return value !== null ? Some(value) : Nil()
-  }
-}
+  () =>
+    Object.assign({ kind: "Nil" }, Maybe)
 
 export {
-  Maybe
+  Maybe,
+  Some,
+  Nil
 }
