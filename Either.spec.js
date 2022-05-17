81142f9229ddff5c5e9e88e8115c72bbac38b158
Tim Biddington <timbiddington@fastmail.com.au>
2022-05-17 20:26:04 +1200

    Upgrade Either.spec to use property based tests

--- Either.spec.js
+++ Either.spec.js
@@ -4,28 +4,41 @@
 
 import { Either, Right, Left } from './Either.js';
 import { identity, compose as comp, map } from './Fn.js';
+import fc from 'fast-check';
 
 describe('Adheres to functor laws', () => {
 
-  test('Either adheres to identity functor law', () => {
-    // fmap id = id
-    let either = Either.of("law")
+  test('Identity :: map id a = a', () => {
 
-    expect(either.map(identity).unwrap()).toBe(either.unwrap());
+    fc.assert(fc.property(fc.string(), s => {
+      let either =
+        Either.of(s)
+              .map(identity)
+      
+      return either.unwrap() === s;
+    }))
   })
 
-  test('Either adheres to composition functor law', () => {
-    // Our functor of 2
-    let fa = Either.of(2)
-    // Our `f` which incs
-    let f =
-      n => n + 1
-    // Our `g` which doubles
-    let g =
-      n => n * 2
-    // Our `fg` which incs then doubles
-    let fg = comp(f,g)
-    // fmap g (fmap f F) = fmap (f • g) F
-    expect(fa.map(f).map(g).unwrap()).toEqual(fa.map(fg).unwrap())
+  test('Composition :: map f . map g = map (f g)', () => {
+
+    fc.assert(fc.property(fc.string(), s => {
+
+      let fa = Either.of(s)
+
+      // Our `f` which incs
+      let f =
+        n => n + 1
+      
+      // Our `g` which doubles                    
+      let g =
+        n => n * 2
+
+      // map f • map g
+      let mapmap = fa.map(f).map(g).unwrap();
+      // map (f • g)
+      let mapcompose = fa.map(comp(f,g)).unwrap();
+
+      expect(mapmap).toEqual(mapcompose);
+    }))
   })
 })
