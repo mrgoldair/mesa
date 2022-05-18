a71023eff28fe5d55055b210e6a614daae584c0c
Tim <tim.biddington@drawboard.com>
2022-05-18 20:51:27 +1200

    Either Spec
    - Applicative Functor
      - Identity, Homomorphism and Composition

--- Either.spec.js
+++ Either.spec.js
@@ -47,6 +47,56 @@ describe('Adheres to functor laws', () => {
 
 describe('Either is an applicative functor', () => {
 
-  test('Identity :: pure f <*> v = v', () => {})
-  test('Homomorphism :: pure f <*> pure a = pure (f a)', () => {})
+  test('Identity :: pure f <*> v = v', () => {
+    fc.assert(fc.property(fc.string(), s => {
+      // pure f <*> v
+      let left = Either.of(identity).ap(Right(s));
+      // v
+      let right = s;
+
+      return left.unwrap() === right;
+    }))
+  })
+
+  test('Homomorphism :: pure f <*> pure a = pure (f a)', () => {
+    fc.assert(fc.property(fc.string(), s => {
+      // pure f <*> pure a
+      let left =
+        Either.of(identity)
+          .ap(Either.of(s))
+      // pure (f a)
+      let right =
+        Either.of(identity(s))
+
+      return left.unwrap() === right.unwrap();
+    }))
+  })
+
+  test('Composition :: (compose) <*> a <*> b <*> c = a <*> (b <*> c)', () => {
+    let excl =
+      s => s + "!"
+  
+    let caps =
+      s => s.toUpperCase()
+
+    fc.assert(fc.property(fc.string(), s => {
+      // (compose) <*> a <*> b <*> c
+      let left =
+        Either.of(comp)
+          .ap(Right(excl))
+          .ap(Right(caps))
+          .ap(Right(s))
+      
+      // a <*> (b <*> c)
+      let right =
+        Right(excl)
+          .ap(Right(caps)
+                .ap(Right(s)))
+
+      return left.unwrap() === right.unwrap()
+    }))
+  })
+
+  test('Interchange :: ', () => {})
+
 })
